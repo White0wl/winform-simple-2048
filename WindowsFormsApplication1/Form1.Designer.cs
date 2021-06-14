68b3f3cf63df8fc2cbaa06536088b84d546992e9
Ryabchenko Andrey <xahtep19977@gmail.com>
2021-06-14 17:36:16 +0300

    adding files

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label0 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(71, 9);
+            this.label1.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label1.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(130, 9);
+            this.label2.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label2.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 57);
+            this.label2.TabIndex = 0;
+            this.label2.Text = " ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(189, 9);
+            this.label3.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label3.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 57);
+            this.label3.TabIndex = 0;
+            this.label3.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 66);
+            this.label4.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label4.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 57);
+            this.label4.TabIndex = 0;
+            this.label4.Text = " ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(71, 66);
+            this.label5.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label5.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 57);
+            this.label5.TabIndex = 0;
+            this.label5.Text = " ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(130, 66);
+            this.label6.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label6.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 57);
+            this.label6.TabIndex = 0;
+            this.label6.Text = " ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(189, 66);
+            this.label7.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label7.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 57);
+            this.label7.TabIndex = 0;
+            this.label7.Text = " ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(12, 123);
+            this.label8.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label8.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 57);
+            this.label8.TabIndex = 0;
+            this.label8.Text = " ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(71, 123);
+            this.label9.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label9.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 57);
+            this.label9.TabIndex = 0;
+            this.label9.Text = " ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(130, 123);
+            this.label10.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label10.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 57);
+            this.label10.TabIndex = 0;
+            this.label10.Text = " ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(189, 123);
+            this.label11.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label11.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 57);
+            this.label11.TabIndex = 0;
+            this.label11.Text = " ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(12, 180);
+            this.label12.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label12.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(53, 57);
+            this.label12.TabIndex = 0;
+            this.label12.Text = " ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(71, 180);
+            this.label13.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label13.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(53, 57);
+            this.label13.TabIndex = 0;
+            this.label13.Text = " ";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label14.Location = new System.Drawing.Point(130, 180);
+            this.label14.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label14.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(53, 57);
+            this.label14.TabIndex = 0;
+            this.label14.Text = " ";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label15.Location = new System.Drawing.Point(189, 180);
+            this.label15.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label15.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(53, 57);
+            this.label15.TabIndex = 0;
+            this.label15.Text = " ";
+            // 
+            // label0
+            // 
+            this.label0.AutoSize = true;
+            this.label0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label0.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label0.Location = new System.Drawing.Point(12, 9);
+            this.label0.MaximumSize = new System.Drawing.Size(53, 57);
+            this.label0.MinimumSize = new System.Drawing.Size(53, 57);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(53, 57);
+            this.label0.TabIndex = 0;
+            this.label0.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 249);
+            this.Controls.Add(this.label0);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label0;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        enum StateCell { CanSum, CantMove, CanMove };
+        //--------------------------------------//
+        private bool SetEmptyCell()
+        {
+            int[] indexes = FoundEmptyCell();
+            int randIdx = -1;
+
+            if (indexes.Length > 0)
+                randIdx = indexes[rand.Next(0, indexes.Length)];
+
+            if (randIdx != -1)
+            {
+                cells[randIdx].Text = "2";
+            }
+            else if (randIdx == -1)
+                return false;
+
+            return true;
+        }
+        private int[] FoundIndicesValuesCell()
+        {
+            List<int> indexes = new List<int>();
+            for (int i = 0; i < 16; i++)
+            {
+                if (cells[i].Text != " ")
+                {
+                    indexes.Add(i);
+                }
+            }
+            return indexes.ToArray();
+        }
+        private int[] ReverseFoundIndicesValuesCell()
+        {
+            int[] indicesValueCells = FoundIndicesValuesCell();
+            int[] tmp = indicesValueCells.ToArray();
+            //Revers
+            for (int i = tmp.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                indicesValueCells[j] = tmp[i];
+            }
+            return indicesValueCells;
+        }
+
+        private int[] FoundEmptyCell()
+        {
+            List<int> indexes = new List<int>();
+            for (int i = 0; i < 16; i++)
+            {
+                if (cells[i].Text == " ")
+                {
+                    indexes.Add(i);
+                }
+            }
+            return indexes.ToArray();
+        }
+
+        private void InitializeArray()
+        {
+            cells.Add(label0);
+            cells.Add(label1);
+            cells.Add(label2);
+            cells.Add(label3);
+            cells.Add(label4);
+            cells.Add(label5);
+            cells.Add(label6);
+            cells.Add(label7);
+            cells.Add(label8);
+            cells.Add(label9);
+            cells.Add(label10);
+            cells.Add(label11);
+            cells.Add(label12);
+            cells.Add(label13);
+            cells.Add(label14);
+            cells.Add(label15);
+        }
+        //--------------------------------------//
+        List<Label> cells;
+        Random rand;
+        bool someChange;
+        DateTime start;
+        int maxValue;
+        public Form1()
+        {
+            cells = new List<Label>(16);
+            start = DateTime.Now;
+            maxValue = 0;
+            InitializeComponent();
+            InitializeArray();
+            someChange = false;
+            rand = new Random();
+
+            for (int i = 0; i < 2; i++)
+                SetEmptyCell();
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    MoveLeftCells();
+                    break;
+                case Keys.Up:
+                    MoveUpCells();
+                    break;
+                case Keys.Right:
+                    MoveRightCells();
+                    break;
+                case Keys.Down:
+                    MoveDownCells();
+                    break;
+                default:
+                    break;
+            }
+
+            if(someChange)
+                SetEmptyCell();
+            someChange = false;
+
+            if (FoundEmptyCell().Length == 0)
+            {
+                MessageBox.Show("Не осталось ячеек за " + (Math.Round((DateTime.Now - start).TotalSeconds,1))+"сек. , максимальное значение - "+maxValue);
+                Close();
+            }
+
+        }
+        private void MoveCell(int idx, string vector = "down")
+        {
+            int idxNow = idx;
+            int idxNext;
+            int vectorIdx = 0;
+
+
+            int minIdx = 0;
+            int maxIdx = 15;
+            
+           
+
+            switch (vector)
+            {
+                case "up":
+                    vectorIdx = -4;
+                    break;
+                case "left":
+                    vectorIdx = -1;
+                    if (idx < 4)
+                    {
+                        minIdx = 0;
+                        maxIdx = 3;
+                    }
+                    else if (idx >= 4 && idx < 8)
+                    {
+                        minIdx = 4;
+                        maxIdx = 7;
+                    }
+                    else if (idx >= 8 && idx < 12)
+                    {
+                        minIdx = 8;
+                        maxIdx = 11;
+                    }
+                    else if (idx >= 12 && idx < 16)
+                    {
+                        minIdx = 12;
+                        maxIdx = 15;
+                    }
+                    break;
+                case "right":
+                    vectorIdx = 1;
+                    if (idx < 4)
+                    {
+                        minIdx = 0;
+                        maxIdx = 3;
+                    }
+                    else if (idx >= 4 && idx < 8)
+                    {
+                        minIdx = 4;
+                        maxIdx = 7;
+                    }
+                    else if (idx >= 8 && idx < 12)
+                    {
+                        minIdx = 8;
+                        maxIdx = 11;
+                    }
+                    else if (idx >= 12 && idx < 16)
+                    {
+                        minIdx = 12;
+                        maxIdx = 15;
+                    }
+                    break;
+                case "down":
+                    vectorIdx = 4;
+                    break;
+                default:
+                    vectorIdx = 0;
+                    break;
+            }
+
+            idxNext = idxNow + vectorIdx;
+            StateCell state;
+            if (idxNext >= minIdx && idxNext <= maxIdx)
+                state = GetStateCell(idxNow, idxNext);
+            else
+                state = StateCell.CantMove;
+
+            while (state != StateCell.CantMove)
+            {
+                if (state == StateCell.CanSum)
+                {
+                    int sum = Int32.Parse(cells[idxNow].Text) + Int32.Parse(cells[idxNext].Text);
+                    if (sum > maxValue)
+                        maxValue = sum;
+                    cells[idxNext].Text = "" + sum;
+                    cells[idxNow].Text = " ";
+                }
+                else if (state == StateCell.CanMove)
+                {
+                    cells[idxNext].Text = cells[idxNow].Text;
+                    cells[idxNow].Text = " ";
+                }
+                idxNow = idxNext;
+                idxNext = idxNow + vectorIdx;
+
+                if (idxNext >= minIdx && idxNext <= maxIdx)
+                    state = GetStateCell(idxNow, idxNext);
+                else
+                    state = StateCell.CantMove;
+                someChange = true;
+            }
+        }
+
+        private StateCell GetStateCell(int idx, int idxNext)
+        {
+            try
+            {
+                if (cells[idx].Text == cells[idxNext].Text)
+                    return StateCell.CanSum;
+                else if (cells[idxNext].Text == " ")
+                    return StateCell.CanMove;
+                else
+                    return StateCell.CantMove;
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                return StateCell.CantMove;
+            }
+        }
+
+        private void MoveDownCells()
+        {
+            int[] indicesValueCells = ReverseFoundIndicesValuesCell();
+            for (int i = 0; i < indicesValueCells.Length; i++)
+            {
+                MoveCell(indicesValueCells[i], "down");
+            }
+        }
+
+
+        private void MoveRightCells()
+        {
+            int[] indicesValueCells = ReverseFoundIndicesValuesCell();
+            for (int i = 0; i < indicesValueCells.Length; i++)
+            {
+                MoveCell(indicesValueCells[i], "right");
+            }
+        }
+
+        private void MoveUpCells()
+        {
+            int[] indicesValueCells = FoundIndicesValuesCell();
+            for (int i = 0; i < indicesValueCells.Length; i++)
+            {
+                MoveCell(indicesValueCells[i], "up");
+            }
+        }
+
+        private void MoveLeftCells()
+        {
+            int[] indicesValueCells = FoundIndicesValuesCell();
+            for (int i = 0; i < indicesValueCells.Length; i++)
+            {
+                MoveCell(indicesValueCells[i], "left");
+            }
+        }
+    }
+}
